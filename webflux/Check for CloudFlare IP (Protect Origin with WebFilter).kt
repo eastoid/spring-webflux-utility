65b564f1593e47cef9110c511f6b61e668162d57
mog <82464360+eastoid@users.noreply.github.com>
2023-08-02 21:34:46 +0200

    Update Check for CloudFlare IP (Protect Origin with WebFilter).kt

--- webflux/Check for CloudFlare IP (Protect Origin with WebFilter).kt
+++ webflux/Check for CloudFlare IP (Protect Origin with WebFilter).kt
@@ -7,77 +7,86 @@
 	CloudFlare IP list: https://www.cloudflare.com/ips/
 	(Imports under code)
 
+
+	Updated:
+ 		A maybe slightly better version of the filter that may or may not be reactive
+		  but a working filter regardless
+
 */
 
 
 
-// --> This configuration (for now) is blocking <--
-To do: cache val, nonblocking code (👺)
+private var ipCache = ConcurrentHashMap<String, Boolean>()
+
+private val IP_RANGES = setOf(
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/13",
+    "104.24.0.0/14",
+    "172.64.0.0/13",
+    "131.0.72.0/22"
+)
 
 
+@OptIn(DelicateCoroutinesApi::class)
 @Component
 class WebfluxFilter : WebFilter {
 
-    private val IP_RANGES = setOf(
-        "173.245.48.0/20",
-        "103.21.244.0/22",
-        "103.22.200.0/22",
-        "103.31.4.0/22",
-        "141.101.64.0/18",
-        "108.162.192.0/18",
-        "190.93.240.0/20",
-        "188.114.96.0/20",
-        "197.234.240.0/22",
-        "198.41.128.0/17",
-        "162.158.0.0/15",
-        "104.16.0.0/13",
-        "104.24.0.0/14",
-        "172.64.0.0/13",
-        "131.0.72.0/22"
-    )
-
-    // cached allowed IPs
-    private var ipCache = ConcurrentHashMap<String, Boolean>()
 
     // filter function
     override fun filter(
         serverWebExchange: ServerWebExchange,
         webFilterChain: WebFilterChain
     ): Mono<Void> {
+        var result: Mono<Void> = Mono.empty()
+        runBlocking {
+            val requestIP = serverWebExchange.request.remoteAddress?.address?.hostAddress ?: ""
+            if (ipCache.count() >= 50) {
+                unCache()
+            }
+            val cloudflareIP = ipCache.computeIfAbsent(requestIP) { ip ->
+                runBlocking {
+                    checkIPInRange(ip).awaitFirstOrNull() ?: false
+                }
+            }
 
-        // get request remote address
-        val requestIP = serverWebExchange.request.remoteAddress?.address?.hostAddress ?: ""
+            println("checking for")
+            if (cloudflareIP) {
+                println("Allowing CloudFlare connection")
 
-        // check if IP is from Cloudflare using cache
-        if (ipCache.count() >= 50) {
-            // check if too many ip cached
-            unCache()
+                val ip = serverWebExchange.request.headers.getFirst("CF-Connecting-IP").toString()
+                if (!ip.contains(".")) {
+                    result = Mono.empty()
+                    
+                }
+
+		result = webFilterChain.filter(serverWebExchange)
+            } else {
+
+
+                println("Refusing direct connection")
+
+		result = Mono.empty()
+
+            }
         }
 
-        val cloudflareIP = ipCache.computeIfAbsent(requestIP) { ip ->
-            checkIPInRange(ip)
-        }
-
-        var ip = ""
-
-        // action if / if not Cloudflare IP
-        if (!cloudflareIP) {
-            return Mono.empty()
-        } else { //action if ip is from CF
-            ip = serverWebExchange.request.headers.getFirst("CF-Connecting-IP").toString()
-            if (!ip.contains(".")) return Mono.empty() // Invalid Cloudflare IP in the header
-        }
-
-
-/*																												<== Process request from CloudFlare  						*/
-
-
-        // Continue filter chain
-        return webFilterChain.filter(serverWebExchange)
+        return result
     }
-    
+
+
+
     // function to remove half of cached ip
-    fun unCache() {
+    suspend fun unCache() {
         try {
             val sortedEntries = ipCache.entries.toList().sortedBy { it.key }
             val splitIndex = ipCache.size / 2
@@ -94,21 +103,22 @@ class WebfluxFilter : WebFilter {
     }
 
     // check if ip is in ip (CF) range
-    fun checkIPInRange(ipAddress: String?): Boolean {
+    suspend fun checkIPInRange(ipAddress: String?): Mono<Boolean> {
         try {
             val address = InetAddress.getByName(ipAddress)
             for (range in IP_RANGES) {
                 val subnetUtils = SubnetUtils(range)
                 if (subnetUtils.info.isInRange(address.hostAddress)) {
-                    return true
+                    return Mono.just(true)
                 }
             }
         } catch (e: Exception) {
             // Log or handle the exception if required
             e.printStackTrace()
         }
-        return false
+        return Mono.just(false)
     }
+
 
 }
 
